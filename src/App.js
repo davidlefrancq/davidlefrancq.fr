9c34020f4701005b7024bf40ed4a75d183762b58
David Lefrancq <david.lefrancq@gmail.com>
2020-06-18 11:43:44 +0200

    [func] React-Router-Dom

--- src/App.js
+++ src/App.js
@@ -4,15 +4,27 @@ import OccurenceList from "./components/OccurenceList";
 import Header from "./components/Header";
 import {Container} from 'react-bootstrap';
 import NewQualification from "./components/edit/NewQualification";
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+
 
 function App() {
-  return (
-    <Container className="App">
-        <Header/>
-        {/*<OccurenceList/>*/}
-        <NewQualification/>
-    </Container>
-  );
+    return (
+        <Router>
+            <Container className="App">
+                <Header/>
+
+                <Switch>
+                    <Route exact path={"/"}>
+                        <OccurenceList/>
+                    </Route>
+                    <Route exact path={"/qualification/new"}>
+                        <NewQualification/>
+                    </Route>
+                </Switch>
+
+            </Container>
+        </Router>
+    );
 }
 
 export default App;
