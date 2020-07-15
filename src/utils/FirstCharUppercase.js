77ed051c500665e138ed448bdfb896de5c338578
David Lefrancq <david.lefrancq@gmail.com>
2020-07-15 16:36:38 +0200

    [fix] First Char Uppercase

--- src/utils/FirstCharUppercase.js
+++ src/utils/FirstCharUppercase.js
@@ -1,7 +1,9 @@
 class FirstCharUppercase {
     static convert(string){
         if(string != undefined && string != null){
-            return string[0].toUpperCase() + string.slice(1);
+            if(string[0] != undefined){
+                return string[0].toUpperCase() + string.slice(1);
+            }
         }
     }
 }
