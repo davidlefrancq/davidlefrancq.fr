750d034a9335ea17054b358694624c371de52748
David Lefrancq <david.lefrancq@gmail.com>
2021-07-13 18:42:15 +0200

    [feat] Rework Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,177 +1,25 @@
-import React, {Component, createRef, Fragment} from 'react';
+import React, {Component, createRef} from 'react';
 import {Jumbotron} from 'react-bootstrap';
-import {connect} from "react-redux";
-import {actions} from '../../actions';
-import {Link} from "react-router-dom";
-import DAOFactory from "../../dal/DAOFactory";
-import LoginForm from "../LoginForm/LoginForm";
 import copy from "copy-to-clipboard";
-import {STRING_LOGIN, STRING_LOGOUT} from "../../translation/fr-fr";
-import {AiOutlineFilePdf, AiOutlineLogin, AiOutlineLogout} from "react-icons/ai";
+import {AiOutlineFilePdf} from "react-icons/ai";
 import "./header.css";
-import {BsArrowBarRight, FaClipboardList, FcRight, FiChevronLeft, FiChevronRight, GoMail} from "react-icons/all";
+import {
+    FaClipboardList,
+    SiGmail,
+    SiLinkedin
+} from "react-icons/all";
 
 class Header extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            connection: {
-                title: STRING_LOGIN,
-                authForm: false,
-                authFormAnimation: "",
-            },
             style: {
-                subtitleAnimation: "",
-                logoAnimation: "",
-                logoFirstAnimation: "",
-                logoLastAnimation: "",
-                loginFormActive: "",
-                msgCopyEmail: "msg-copy-email-disabled",
+                 msgCopyEmail: "msg-copy-email-disabled",
             },
             email: "@EMail",
         };
         this.refEmail = createRef();
-    }
-
-    openFormAuth = () => {
-        const state = {...this.state};
-        state.connection.authForm = true;
-        // state.connection.authFormAnimation = "auth-form-on";
-        state.style.loginFormActive = "active-animation";
-        this.setState(state);
-    }
-
-    closeFormAuth = () => {
-        const state = {...this.state};
-        state.connection.authForm = false;
-        // state.connection.authFormAnimation = "auth-form-off";
-        state.style.loginFormActive = "inactive-animation";
-        this.setState(state);
-    }
-
-    logout = () => {
-        const {setToken, setAuthentification} = this.props;
-        const state = {...this.state};
-        setToken(null);
-        setAuthentification(false);
-        state.connection.title = STRING_LOGIN;
-        state.connection.authForm = false;
-        // state.connection.authFormAnimation = "";
-        this.setState(state);
-    }
-
-    handleAuthForm = () => {
-        const {isLoggedIn} = this.props;
-        const {authForm} = this.state.connection;
-
-        if (isLoggedIn) {
-            this.logout();
-        } else {
-            if (!authForm) {
-                this.openFormAuth();
-
-            } else {
-                this.closeFormAuth();
-            }
-        }
-    }
-
-    handleAuthentificationSuccess = (res) => {
-
-        const state = {...this.state};
-        const {setAuthentification, setToken} = this.props;
-        const token = res.data.token;
-
-        setToken(token);
-        setAuthentification(true);
-        state.connection.title = STRING_LOGOUT;
-
-        this.setState(state);
-    }
-
-    handleAuthentificationFailure = (res) => {
-
-        const {setAuthentification} = this.props;
-        const state = {...this.state};
-
-        state.connection.authForm = false;
-        this.setState(state);
-
-        setAuthentification(false);
-    }
-
-    handleAuthentification = (email, password) => {
-
-        const {setAuthentification, isLoggedIn, setToken} = this.props;
-        const daoFactory = new DAOFactory();
-        const state = {...this.state};
-
-        if (!isLoggedIn) {
-            // state.connection.authFormAnimation = "auth-form-off";
-            this.setState(state);
-
-            daoFactory
-                .getAuthenticationDAO()
-                .login(email, password)
-                .then(this.handleAuthentificationSuccess, this.handleAuthentificationFailure)
-            ;
-        } else {
-            setToken(null);
-            setAuthentification(false);
-            state.connection.title = STRING_LOGIN;
-            state.connection.authForm = false;
-            this.setState(state);
-        }
-    }
-
-    renderAuthentificationLabel() {
-        const {authForm} = this.state.connection;
-        if (authForm) {
-            return <AiOutlineLogin style={{fontSize: "x-large"}}/>;
-        } else {
-            return <AiOutlineLogout style={{fontSize: "x-large"}}/>;
-        }
-    }
-
-    renderButtonHome() {
-        const {isLoggedIn} = this.props;
-        if (isLoggedIn) {
-            return (
-                <li className="nav-item">
-                    <Link className="nav-link" to="/">Accueil</Link>
-                </li>
-            );
-        }
-    }
-
-    renderButtonAdmin() {
-        const {isLoggedIn} = this.props;
-        if (isLoggedIn) {
-            return (
-                <li className="nav-item">
-                    <Link className="nav-link" to="/admin">Admin</Link>
-                </li>
-            );
-        }
-    }
-
-    logoEnterAnimation = () => {
-        const state = {...this.state};
-        state.style.subtitleAnimation = "subtitle-hover"
-        state.style.logoAnimation = "logo-hover"
-        state.style.logoFirstAnimation = "logo-first-action-hover"
-        state.style.logoLastAnimation = "logo-last-action-hover"
-        this.setState(state);
-    }
-
-    logoLeaveAnimation = () => {
-        const state = {...this.state};
-        state.style.subtitleAnimation = "subtitle-leave"
-        state.style.logoAnimation = "logo-leave"
-        state.style.logoFirstAnimation = "logo-first-action-leave"
-        state.style.logoLastAnimation = "logo-last-action-leave"
-        this.setState(state);
     }
 
     copyEmail = () => {
@@ -183,8 +31,8 @@ class Header extends Component {
             const state = {...this.state};
             state.style.msgCopyEmail = "msg-copy-email";
             this.setState(state);
-            setTimeout(this.hideMsgCopyEmail,5000);
-        }catch (error){
+            setTimeout(this.hideMsgCopyEmail, 1000);
+        } catch (error) {
             // console.log(error);
         }
     }
@@ -195,113 +43,79 @@ class Header extends Component {
     }
 
     render() {
-        const {isLoggedIn} = this.props;
-        const {authFormAnimation} = this.state.connection;
-        const {
-            subtitleAnimation,
-            logoFirstAnimation,
-            logoLastAnimation,
-            logoAnimation,
-            loginFormActive
-        } = this.state.style;
-        let dNone = "";
-        if (isLoggedIn) {
-            dNone = "d-none";
-        }
-
 
         return (
-            <Fragment>
+            <div
+                className={"shadow"}
+            >
 
-                <nav className="navbar navbar-expand-sm navbar-dark bg-primary p-0 mt-0 ml-0 mr-0 mb-4">
-                    <Link className={"col-auto navbar-brand p-0 m-0"} to={"/"}>
-                        {/*<a className="col-auto navbar-brand p-0 m-0" href="/">*/}
-                        <div className={`logo ${logoAnimation}`}
-                             onMouseEnter={this.logoEnterAnimation}
-                             onMouseLeave={this.logoLeaveAnimation}
-                        >
-                            <div className={"initial"}>D</div>
-                            <div className={`first-name ${logoFirstAnimation}`}>avid</div>
-                            <div className={"initial"}>L</div>
-                            <div className={`last-name ${logoLastAnimation}`}>efrancq</div>
+                <Jumbotron className={"mb-0 pt-4 pb-3 shadow-none border-bottom"}>
+                    <div className={"row"}>
+                        <div className={"col-auto mt-3"}>
+                            {"Class Title { "}
                         </div>
-                        {/*</a>*/}
-                    </Link>
-
-                    {/*<h1 className={"col-auto ml-3 text-white"}>Concepteur Développeur Informatique</h1>*/}
-
-                    <ul className="navbar-nav mr-auto ml-auto menu">
-                        {this.renderButtonHome()}
-                        {this.renderButtonAdmin()}
-                    </ul>
-
-                    {/*<div className={`navbar-nav login-form auth-form ${authFormAnimation} ${dNone}`}>*/}
-                    <div className={`navbar-nav login-form ${loginFormActive} ${dNone}`}>
-                        <form>
-                            <LoginForm handleAuthentification={this.handleAuthentification}/>
-                        </form>
+                        <h1 className={"col-auto"}>
+                            Concepteur Développeur Informatique
+                        </h1>
+                        <div className={"col-auto mt-3"}>
+                            {"}"}
+                        </div>
+                    </div>
+                    <div className={"row"}>
+                        <div className={"col-auto mt-3"}>
+                            {"Class Name { "}
+                        </div>
+                        <div className={"col-auto"}>
+                            <p style={{fontSize: "xx-large"}}>
+                                David Lefrancq
+                            </p>
+                        </div>
+                        <div className={"col-auto mt-3"}>
+                            {"}"}
+                        </div>
                     </div>
 
-                    <ul className="navbar-nav">
-                        <li className="nav-item" onClick={this.handleAuthForm} style={{userSelect: "none"}}>
-                            <a className="nav-link">
-                                {this.renderAuthentificationLabel()}
-                            </a>
-                        </li>
-                    </ul>
-
-                </nav>
-
-                <Jumbotron className={"pt-4"}>
-                    <h1 className={``}>Concepteur Développeur Informatique</h1>
-                    <p className={`${subtitleAnimation}`}>David Lefrancq</p>
-
                     <div className={"utils"}>
-                        <a className={"btn btn-danger"} href={"./David LEFRANCQ - CV Web.pdf"} target={"_blank"}>
-                            {/*<MdPictureAsPdf/>*/}
-                            <AiOutlineFilePdf className={"mb-2"}/>
-                            <span className={"ml-2"}>CV</span>
+                        <a
+                            className={"p-1 btn btn-danger"}
+                            style={{minHeight: 50}}
+                            href={"https://drive.google.com/file/d/1iLfuZhBWsX3EjiNebaD-JhMivWHztsfs/view?usp=sharing"}
+                            target={"_blank"}
+                        >
+                            <AiOutlineFilePdf/>
                         </a>
 
-                        <button className={"btn btn-primary"} onClick={this.copyEmail}>
-                            <span ref={this.refEmail}>
-                                {/*{this.state.email}*/}
-                                <GoMail/>
-                                {/*<BsArrowBarRight/>*/}
-                                {/*<FcRight/>*/}
-                                {/*<FiChevronRight/>*/}
+                        <a
+                            className={"p-1 btn btn-primary"}
+                            style={{minHeight: 50}}
+                            href={"https://www.linkedin.com/in/david-lefrancq/"}
+                            target={"_blank"}
+                        >
+                            <SiLinkedin/>
+                        </a>
+
+                        <button className={"p-1 btn btn-primary"} onClick={this.copyEmail}
+                                style={{minHeight: 50}}
+                        >
+                            <span ref={this.refEmail} className={"m-1"}>
+                                <SiGmail size={14}/>
                             </span>
                         </button>
-                        <div className={`p-3 ${this.state.style.msgCopyEmail}`}>
-                            <div className={"h3 border-bottom pb-1"}>
-                                <FaClipboardList/>
-                                <FiChevronLeft/>
-                                <GoMail/>
-                            </div>
-                            Mon EMail à été copié dans votre presse papier.
+
+                        <div className={`p-3 ${this.state.style.msgCopyEmail}`}
+                             style={{minHeight: 50}}
+                        >
+
+                            <span className={"mr-1"}><span className={"font-weight-bold"}>@email</span> copier dans le presse papier</span>
+                            <FaClipboardList style={{marginBottom: 3}}/>
                         </div>
                     </div>
 
                 </Jumbotron>
 
-            </Fragment>
+            </div>
         );
     }
 }
 
-
-const mapStateToProps = (state) => {
-    return {
-        isLoggedIn: state.AuthentificationReducer.isLoggedIn,
-        token: state.AuthentificationReducer.token,
-    };
-}
-
-const mapDispatchToProps = (dispatch) => {
-    return {
-        setAuthentification: (isLoggedIn) => dispatch(actions.authentification.setAuthentification(isLoggedIn)),
-        setToken: (token) => dispatch(actions.authentification.setToken(token)),
-    };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Header);
+export default Header;
