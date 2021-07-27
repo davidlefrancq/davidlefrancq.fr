cab092997ab4326933553c4288cf8c6b2c785342
David Lefrancq <david.lefrancq@gmail.com>
2021-07-27 13:44:41 +0200

    [feat] Polish GUI

--- src/components/Occurrence/InfoEntity.js
+++ src/components/Occurrence/InfoEntity.js
@@ -12,7 +12,7 @@ const InfoEntity = (props) => {
     const renderLink = () => {
         if (urlTitle && urlTitle != "") {
             return (
-                <div className={"mt-4"}>
+                <div className={"mt-4 p-2 rounded"} style={{backgroundColor: "rgba(0,0,0,0.5)"}}>
                     <a className={"ml-3 mr-3"} href={url} target={"_blank"}>
                         <GrFirefox size={24}/>
                         <span style={{marginLeft: 5}}>{urlTitle}</span>
@@ -22,20 +22,30 @@ const InfoEntity = (props) => {
         }
     };
 
+    const renderGoogleMapLink = () => {
+        if ((city && city != "")|| (Number.parseInt(lat) && Number.parseInt(lng))) {
+            return (
+                <div className={"mt-4 p-2 rounded"} style={{backgroundColor: "rgba(0,0,0,0.5)"}}>
+                    <div className={"ml-3"}>
+                        <LinkGoogleMap address={address} postalCode={postalCode} city={city} lat={lat} lng={lng}/>
+                    </div>
+                </div>
+            );
+        }
+    }
+
     return (
         <div className={`pt-2 ${animated}`}>
 
-            <LogoEntity url={url} logo={logo}/>
-            <h3 className={"mt-2"} style={{fontSize: "large"}}>{name}</h3>
-            <div>{postalCode} {city}</div>
+            <div className={"p-2 rounded"} style={{backgroundColor: "rgba(0,0,0,0.5)"}}>
+                <LogoEntity url={url} logo={logo}/>
+                <h3 className={"mt-2"} style={{fontSize: "large"}}>{name}</h3>
+                <div>{postalCode} {city}</div>
+            </div>
 
             {renderLink()}
 
-            <div className={"mt-4"}>
-                <div className={"ml-3"}>
-                    <LinkGoogleMap address={address} postalCode={postalCode} city={city} lat={lat} lng={lng}/>
-                </div>
-            </div>
+            {renderGoogleMapLink()}
 
         </div>
     );
